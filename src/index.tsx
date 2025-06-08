674d767ca81f29d9d6c4626332d9244d9dd4d8e4
Jax Engel <6673768+jaxatto@users.noreply.github.com>
2025-06-08 02:27:07 -0500

    update dep

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import Home from './pages/Home';
 import './styles/main.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>
+  );
+}
