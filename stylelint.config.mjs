86c14d4ea31f47427e020f9612ba04008b8f570c
Jax Engel <jaxengelofficial@gmail.com>
2026-08-24 21:11:43 -0500

    add style lint and paths

---
+++ stylelint.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('stylelint').Config} */
+export default {
+	extends: ['stylelint-config-standard'],
+
+	referenceFiles: ['src/styles/variables.css'],
+
+	rules: {
+		'comment-empty-line-before': null,
+		'custom-property-no-missing-var-function': true,
+		'no-unknown-custom-properties': true,
+	},
+};
