90cd9bb7046b1c2d4e6907348d71af0fb31257aa
Jax Engel <6673768+jaxatto@users.noreply.github.com>
2026-09-06 23:29:28 -0500

    Update resume header and page footer

--- src/components/Footer/resources/content.ts
+++ src/components/Footer/resources/content.ts
@@ -1,19 +1,19 @@
-import { contactInfo } from '@constants/contactInfo';
+import { contactInfo } from "@constants/contactInfo";
 
 export const content = {
-    title: 'Let\'s connect',
-    description: 'Currently tinkering with design tokens at Toyota. Always open to talk design.',
-    email: {
-        preText: 'Email me at',
-        label: contactInfo.email.label,
-        src: contactInfo.email.src,
-        srOnly: '(opens email client)',
-    },
-    linkedin: {
-        preText: 'Find me on',
-        label: 'LinkedIn',
-        src: contactInfo.linkedin.src,
-        srOnly: '(opens in a new tab)',
-    },
-
-}
+  title: "Let's connect",
+  description:
+    "Looking for remote Staff Product Designer or Design Technologist roles.",
+  email: {
+    preText: "Email me at",
+    label: contactInfo.email.label,
+    src: contactInfo.email.src,
+    srOnly: "(opens email client)",
+  },
+  linkedin: {
+    preText: "Find me on",
+    label: "LinkedIn",
+    src: contactInfo.linkedin.src,
+    srOnly: "(opens in a new tab)",
+  },
+};
--- src/pages/Resume/components/Header/Header.tsx
+++ src/pages/Resume/components/Header/Header.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import Link from '@components/Link';
-import styles from './Header.module.scss';
-import { content } from './resources/content';
+import React from "react";
+import Link from "@components/Link";
+import styles from "./Header.module.scss";
+import { useSiteConfig } from "@providers/SiteConfig";
+import { content } from "./resources/content";
 
-const Header: React.FC = () => (
-  <section className={styles.wrapper}>
-    <h1 className="sr-only">{content.h1}</h1>
-    
-    <h2 className={styles['page-title-lg']}>{content.heading}</h2>
-    <p className={styles.description}>
-      <span className={styles.description1}>{content.description1}</span>
-      <span className={styles.description2}>{content.description2}</span>
-    </p>
+const Header: React.FC = () => {
+  const { isJobSeeking } = useSiteConfig();
 
-    {/* <p className={styles.byline}>{content.byline}</p>
-    <Link href={content.buttonHref} className={styles.button} iconName="arrow-right" styleAs="button" newTab={true}>
-      {content.buttonLabel}
-    </Link> */}
-  </section>
-);
+  return (
+    <section className={styles.wrapper}>
+      <h1 className="sr-only">{content.h1}</h1>
+
+      <h2 className={styles["page-title-lg"]}>{content.heading}</h2>
+      <p className={styles.description}>
+        <span className={styles.description1}>{content.description1}</span>
+        <span className={styles.description2}>{content.description2}</span>
+      </p>
+
+      {isJobSeeking ? (
+        <>
+          <p className={styles.byline}>{content.byline}</p>
+          <Link
+            href={content.buttonHref}
+            className={styles.button}
+            iconName="arrow-right"
+            styleAs="button"
+            newTab={true}
+          >
+            {content.buttonLabel}
+          </Link>
+        </>
+      ) : null}
+    </section>
+  );
+};
 
 export default Header;
